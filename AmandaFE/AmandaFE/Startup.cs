070cca761e815afedb5e8ef3bd790bcc6ff90634
ErmineNotyours <arthurallen@centurylink.net>
2018-04-16 16:02:03 -0700

    Ready for migration

--- AmandaFE/AmandaFE/Startup.cs
+++ AmandaFE/AmandaFE/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AmandaFE.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -26,9 +27,8 @@ namespace AmandaFE
         {
             services.AddMvc();
 
-            services.AddDbContext<BlogDbContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
+            services.AddDbContext<BlogDBContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
