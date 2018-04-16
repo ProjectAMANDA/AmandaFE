4dc1610739ce36eaa697933a23ec2ed7cf942a30
philipwerner <philip.r.werner@gmail.com>
2018-04-16 16:12:12 -0700

    commit for commits sake

---
+++ AmandaFE/AmandaFE/Models/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmandaFE.Models
+{
+    public class UserViewModel
+    {
+        public List<User> users;
+        public SelectList name;
+    }
+}
