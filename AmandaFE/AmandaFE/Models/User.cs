19af47ac6a144490f6f20e0e3e6390cf4390bdf6
ErmineNotyours <arthurallen@centurylink.net>
2018-04-16 12:13:22 -0700

    Added properties to User and Post

--- AmandaFE/AmandaFE/Models/User.cs
+++ AmandaFE/AmandaFE/Models/User.cs
@@ -7,5 +7,8 @@ namespace AmandaFE.Models
 {
     public class User
     {
+        public int PostIds { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
