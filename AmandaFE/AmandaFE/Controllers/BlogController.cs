931ea2c867879d3eae853a16e91490fa11e1153e
Joshua Taylor <taylor.joshua88@gmail.com>
2018-04-17 10:30:30 -0700

    Added Cookies static class, added context to BlogController

--- AmandaFE/AmandaFE/Controllers/BlogController.cs
+++ AmandaFE/AmandaFE/Controllers/BlogController.cs
@@ -3,14 +3,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AmandaFE.Models;
+using AmandaFE.Data;
 
 namespace AmandaFE.Controllers
 {
     public class BlogController : Controller
     {
+        private readonly BlogDBContext _context;
+
+        public BlogController(BlogDBContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            Post post = new Post();
+
+            if (Request.Cookies.ContainsKey(Cookies.UserId) &&
+                Int32.TryParse(Request.Cookies[Cookies.UserId], out int userId))
+            {
+                User user = _context.User.FirstOrDefault(u => u.Id == userId);
+                post.UserId = userId;
+                post.User = user;
+            }
         }
     }
 }
---
+++ AmandaFE/AmandaFE/Cookies.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmandaFE
+{
+    public static class Cookies
+    {
+        public static string UserId { get => "userId"; }
+    }
+}
