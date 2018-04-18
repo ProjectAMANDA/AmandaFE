b8299a3b6b2226838321594e3812e3cd6a11942c
Joshua Taylor <taylor.joshua88@gmail.com>
2018-04-18 10:48:59 -0700

    Started unit testing

---
+++ AmandaFE/FrontendTesting/ErrorViewModelTest.cs
@@ -0,0 +1,41 @@
+﻿using AmandaFE.Models;
+using System;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Extensions;
+using Xunit.Sdk;
+
+namespace FrontendTesting
+{
+    public class ErrorViewModelTest
+    {
+        [Fact]
+        public void CanGetRequestId()
+        {
+            // Arrange
+            ErrorViewModel vm = new ErrorViewModel()
+            {
+                RequestId = "Hello, world!"
+            };
+
+            // Assert
+            Assert.Equal("Hello, world!", vm.RequestId);
+        }
+
+        [Fact]
+        public void CanSetRequestId()
+        {
+            // Arrange
+            ErrorViewModel vm = new ErrorViewModel()
+            {
+                RequestId = "Hello, world!"
+            };
+
+            // Act
+            vm.RequestId = "Goodbye";
+
+            // Assert
+            Assert.Equal("Goodbye", vm.RequestId);
+        }
+    }
+}
