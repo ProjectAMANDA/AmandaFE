9befa6b85c5c99dbbb4163e1a61e49aac783b542
wtnerb <wtnerb@gmail.com>
2018-04-19 11:11:58 -0700

    Can get keywords from parallel dots

---
+++ AmandaFE/AmandaFE/ParallelDotsAPI.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using ParallelDots;
+
+namespace AmandaFE
+{
+    public class ParallelDotsAPI
+    {
+        public static IEnumerable<string> GetTags(string text)
+        {
+            paralleldots pd = new paralleldots("kB9ZXk2RErNglHtNOCLMqCb0S7SDUBDtZJ5B5dJbWMU");
+            JToken keywords = JObject.Parse(pd.keywords(text));
+            return keywords["keywords"].OrderByDescending(k => k["confidence_score"]).Select(k => (string)k["keyword"]).Take(3);
+        }
+    }
+}
