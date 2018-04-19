08506045e475d75c60e2c4ae90a090e6ad666ce8
wtnerb <wtnerb@gmail.com>
2018-04-19 11:13:01 -0700

    format fixt

--- AmandaFE/AmandaFE/ParallelDotsAPI.cs
+++ AmandaFE/AmandaFE/ParallelDotsAPI.cs
@@ -11,7 +11,9 @@ namespace AmandaFE
         {
             paralleldots pd = new paralleldots("kB9ZXk2RErNglHtNOCLMqCb0S7SDUBDtZJ5B5dJbWMU");
             JToken keywords = JObject.Parse(pd.keywords(text));
-            return keywords["keywords"].OrderByDescending(k => k["confidence_score"]).Select(k => (string)k["keyword"]).Take(3);
+            return keywords["keywords"].OrderByDescending(k => k["confidence_score"])
+                                       .Select(k => (string)k["keyword"])
+                                       .Take(3);
         }
     }
 }
