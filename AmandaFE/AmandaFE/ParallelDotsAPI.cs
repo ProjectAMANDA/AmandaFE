f31116ef4326dfc639b9d410c71c50e81d751a4f
wtnerb <wtnerb@gmail.com>
2018-04-19 18:12:10 -0700

    rec Keywords

--- AmandaFE/AmandaFE/ParallelDotsAPI.cs
+++ AmandaFE/AmandaFE/ParallelDotsAPI.cs
@@ -1,19 +1,29 @@
 ﻿using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.Linq;
-using ParallelDots;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace AmandaFE
 {
     public class ParallelDotsAPI
     {
-        public static IEnumerable<string> GenerateTags(string text)
+        public async static Task<IEnumerable<string>> GenerateTagsAsync(string text)
         {
-            paralleldots pd = new paralleldots("kB9ZXk2RErNglHtNOCLMqCb0S7SDUBDtZJ5B5dJbWMU");
-            JToken keywords = JObject.Parse(pd.keywords(text));
-            return keywords["keywords"].OrderByDescending(k => k["confidence_score"])
+            List<string> words = new List<string>();
+            string uri = $"https://apis.paralleldots.com/v2/keywords?text={text}&api_key=kB9ZXk2RErNglHtNOCLMqCb0S7SDUBDtZJ5B5dJbWMU";
+            using (var client = new HttpClient())
+            {
+                //HttpContent thing 
+                var response = await client.PostAsync(uri, new StringContent(""));
+                string responseString = await response.Content.ReadAsStringAsync();
+                JToken keywords = JObject.Parse(responseString);
+                words = keywords["keywords"].OrderByDescending(k => k["confidence_score"])
                                        .Select(k => (string)k["keyword"])
-                                       .Take(3);
+                                       .Take(3)
+                                       .ToList();
+            }
+            return words;
         }
     }
 }
